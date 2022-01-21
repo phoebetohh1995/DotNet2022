e0cd8ec4dcf6d68c3f6159aa1b584a5d40d725fe
phoebetohh1995 <phoebetohh@gmail.com>
2022-01-21 15:58:48 +0800

    Update assignment

---
+++ Day 5/ManageCustomer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PizzaModels_Library;
+
+namespace PizzaFEConsoleApp
+{
+    internal class ManageCustomer
+    {
+        Customer customer = new Customer();
+        public void RegisterCustomer()
+        {
+
+            customer.TakeCustomerDetailsFromUser();
+        }
+        public void DisplayCustomer()
+        {
+            customer.PrintCustomerDetails();
+        }
+
+
+    }
+}
---
+++ Day 5/UnderstandingInheritance/UnderstandingInheritance/Phone.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandingInheritance
+{
+    internal class Phone
+    {
+        public string PhoneNumber { get; set; }
+        public Phone()
+        {
+            PhoneNumber = "9876543";
+            Console.WriteLine("Hey its a phone");
+        }
+        public void MakeCall()
+        {
+            Console.WriteLine("We can make calls from teh number " + PhoneNumber);
+        }
+        public void ReceiveCall()
+        {
+            Console.WriteLine("Tring .... Tring");
+            Console.WriteLine("We can receive calls  ");
+        }
+    }
+}
